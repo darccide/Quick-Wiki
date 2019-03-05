abb4bf0ad2c2aa750c2b31807ad41a1ad5f21b31
Steven Thomson <darccide@protonmail.com>
2019-03-05 22:54:55 +0900

    make it better

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -6,9 +6,9 @@ module.exports = {
   index(req, res, next) {
     wikiQueries.getAllWikis((err, wikis) => {
       if (err) {
-        res.redirect(500, 'static/index');
+        res.redirect(500, '/');
       } else {
-        res.render('wikis/index', { wikis });
+        res.render('wikis/wiki', { wikis });
       }
     });
   },
